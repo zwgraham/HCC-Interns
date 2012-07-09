1abfd411b4a2a778086fe56a6ea303c394e93f56
Zachary W. Graham <zack.graham@gmail.com>
2012-07-09 00:08:31 -0700

    Initial commit for ChargePoint code

---
+++ ChargePoint/chargepoint.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from suds.client import Client
+from suds.wsse import Security, UsernameToken
+
+
+class ChargePointAPIAccount(object):
+    '''
+    Account Information
+    '''
+    def __init__(self, key, pw, url):
+        self.client=Client(url)
+        self.security=Security()
+        self.security.tokens.append(UsernameToken(key,pw))
+        self.client.set_options(wsse=self.security)
+
+
+class StationInformation(ChargePointAPIAccount):
+    '''
+    Access the ChargePoint Network Station Information API
+    '''
+    def __init__(self, key, pw):
+        url='https://webservices.chargepointportal.net:8081/coulomb_api_1.1.wsdl'
+        ChargePointAPIAccount.__init__(self,key,pw,url)
+        self.connect()
+    
+    def getUSStationInformation(self, Name=None, Address=None, City=None, State=None, PostalCode=None, Lat=None, Long=None, ProximityUnit='M'):
+        pass
+
+
+
+if __name__ == "__main__":
+    import ConfigParser
+    try:
+        Config = ConfigParser.ConfigParser()
+        Config.read('./hcc.conf')
+        key=Config.get('Account', 'key')
+        pw=Config.get('Account', 'pw')
+#        url=Config.get('Account', 'url')
+    except:
+        print "Exception parsing config file"
+
+    s=StationInformation(key,pw)
