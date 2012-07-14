b72c138fd069936603cfa0941f666cff01e8361e
Zachary W. Graham <zack.graham@gmail.com>
2012-07-14 00:19:59 -0700

    stuff

--- windparser/windparser.py
+++ windparser/windparser.py
@@ -10,17 +10,25 @@ def str2datetime(x,y):
 
 
 
-starttime=datetime.datetime(2012,01,01,00,00)
-INPUT='201201wind.csv'
-OUTPUT='pocessed.wnd'
 
-infile=csv.reader(file(INPUT, 'r'))
+
+
+starttime=datetime.datetime(2012,01,01,00,00)
+
+INPUT=sys.argv[1]+'.csv'
+OUTPUT=sys.argv[1]+'.wnd'
+#INPUT='01jan2012.csv'
+#OUTPUT='jan.wnd'
+f=file(INPUT,'r')
+infile=csv.reader(f)
 headers=infile.next()
 
 #ensure we have the correct indices
 D=headers.index('Date')
 T=headers.index('Time')
 W=headers.index('WindSpeed')
+
+outfile=file(OUTPUT,'w')
 
 temp_array=[]
 working_datetime=starttime
@@ -38,13 +46,14 @@ for row in infile:
         #moved to new timeslot,
         ##process old data
         if len(temp_array)==0:
-            sys.stdout.write(str(working_datetime)+'\t%.2f\t%d\n'%(0.0,0))
+            averageWindspeed=0.0
         else:
             averageWindspeed=sum(temp_array)/float(len(temp_array))
-            averageWindspeed*=0.44704 #mph to m/s
-            sys.stdout.write(str(working_datetime)+'\t%.2f\t%d\n'%(averageWindspeed, len(temp_array)))
         ##convert to m/s
+        averageWindspeed*=0.44704 #mph to m/s
+        sys.stdout.write(str(working_datetime)+'\t%.2f\t%d\n'%(averageWindspeed, len(temp_array)))
         ##write to file
+        outfile.write('%.4f\n'%(averageWindspeed))
         ##reset variables
         working_datetime=working_datetime+reltime
         temp_array=[]
@@ -57,6 +66,9 @@ for row in infile:
         print 'ERROR'
         print row
         break
+
+outfile.close()
+f.close()
         
 
 
