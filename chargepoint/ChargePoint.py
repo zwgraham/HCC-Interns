104acc5d84054d4583de286857239efdcb6952f6
Zachary W. Graham <zack.graham@gmail.com>
2012-07-16 11:02:27 -0700

    retabbed the files so that \t is 4 spaces

--- chargepoint/ChargePoint.py
+++ chargepoint/ChargePoint.py
@@ -26,41 +26,41 @@ class StationInformation(ChargePointAPIAccount):
         ChargePointAPIAccount.__init__(self,key,pw,url)
 
     def SearchByZipCode(self, ZipCode='93906', Proximity=15, ProximityUnit='M'):
-	self.logger.info('Searching by Zipcode(%s)'%(ZipCode))
+        self.logger.info('Searching by Zipcode(%s)'%(ZipCode))
         searchRequest = self.client.factory.create('stationSearchRequest')
         searchRequest.postalCode=ZipCode
         searchRequest.Proximity=Proximity
         searchRequest.proximityUnit=ProximityUnit
         reply=self.client.service.getAllUSStations(searchRequest)
-	self.logger.debug('API returned code %{0}'.format(reply.responseCode))
+        self.logger.debug('API returned code %{0}'.format(reply.responseCode))
         #needs error checking by response code
         stationsInZipCode=[]
         for station in reply.stationData:
             if station.postalCode == ZipCode:
                 stationsInZipCode.append(station)
-	self.logger.info('API Returned %d results %d of which were in %s'%(len(reply.stationData),len(stationsInZipCode),ZipCode))
+        self.logger.info('API Returned %d results %d of which were in %s'%(len(reply.stationData),len(stationsInZipCode),ZipCode))
         return stationsInZipCode
 
     def SearchByGeo(self, Latitude='36.673192', Longitude = '-121.605089', Proximity=20, ProximityUnit='M'):
-	self.logger.info('Searching by GPS(%s,%s)'%(Latitude,Longitude))
+        self.logger.info('Searching by GPS(%s,%s)'%(Latitude,Longitude))
         searchRequest = self.client.factory.create('stationSearchRequest')
         searchRequest.Geo.lat=Latitude
         searchRequest.Geo.long=Longitude
         searchRequest.Proximity=Proximity
         searchRequest.proximityUnit=ProximityUnit
         reply=self.client.service.getAllUSStations(searchRequest)
-	self.logger.debug('API returned code %s: $s'%(reply.responseCode,reply.responseText))
+        self.logger.debug('API returned code %s: $s'%(reply.responseCode,reply.responseText))
         #needs error checking by response code
         stationsByGeo=[]
-	#can't we just return reply.stationData?
-	#either "return reply.stationData" or "stationsByGeo=reply.stationData"
+        #can't we just return reply.stationData?
+        #either "return reply.stationData" or "stationsByGeo=reply.stationData"
         for station in reply:
             stationsByGeo.append(station)
-	self.logger.info('API Returned %d results'%(len(stationsByGeo)))
+        self.logger.info('API Returned %d results'%(len(stationsByGeo)))
         return stationsByGeo
     
     def searchByAddress(self, Address='1743 East Alisal St', City='Salinas', State='CA', Proximity=15, ProximityUnit='M'):
-	self.logger.info('Searching by address:  %s, %s %s'%(Address,City,State))
+    self.logger.info('Searching by address:  %s, %s %s'%(Address,City,State))
         searchRequest = self.client.factory.create('stationSearchRequest')
         searchRequest.Address = Address
         searchRequest.City= City
@@ -68,12 +68,12 @@ class StationInformation(ChargePointAPIAccount):
         searchRequest.Proximity=Proximity
         searchRequest.proximityUnit=ProximityUnit
         reply=self.client.service.getAllUSStations(searchRequest)
-	self.logger.debug('API returned code %d: $s'%(int(reply.responseCode),reply.responseText))
+        self.logger.debug('API returned code %d: $s'%(int(reply.responseCode),reply.responseText))
         stationByAddress = []
-	#can't we just return reply.stationData?
+        #can't we just return reply.stationData?
         for station in reply.stationData:
             stationByAddress.append(station)
-	self.logger.info('API Returned %d results' %(len(stationByAddress)))
+        self.logger.info('API Returned %d results' %(len(stationByAddress)))
         return stationByAddress
 
 
@@ -84,7 +84,7 @@ if __name__ == "__main__":
         Config = ConfigParser.ConfigParser()
         Config.read('./hcc.conf')
     except:
-	print "Exception reading config file"
+    print "Exception reading config file"
     try:
         key=Config.get('Account', 'key')
         pw=Config.get('Account', 'password')
