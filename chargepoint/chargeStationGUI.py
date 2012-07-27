3c888bebcbb82a8089408c1e092b70ce2a7a6a5a
conorkaminer <conorkaminer@gmail.com>
2012-07-26 21:58:19 -0700

    Now have both text inputs and toolbar functioning together.

--- chargepoint/chargeStationGUI.py
+++ chargepoint/chargeStationGUI.py
@@ -10,36 +10,38 @@ import ChargePoint
 class ChargeStationFind(wx.Frame):
 
 	def __init__(self, parent, title):
-		super(ChargeStationFind, self).__init__(parent, title=title, size = (260,180))
+		super(ChargeStationFind, self).__init__(parent, title=title, size = (500,400))
 		self.InitUI()
 
 	def InitUI(self):
-		#toolbar = self.CreateToolBar()
-		#exitpng = wx.Bitmap('texit.png')
-		#exitpng.SetSize((10,10))
-		#qtool = toolbar.AddLabelTool(wx.ID_ANY, 'Quit', wx.Bitmap('texit.png'))
-		#toolbar.Realize()
-		#self.Bind(wx.EVT_TOOL, self.OnQuit, qtool)
+		toolbar = self.CreateToolBar()
+		exitpng = wx.Bitmap('texit.png')
+		exitpng.SetSize((10,10))
+		qtool = toolbar.AddLabelTool(wx.ID_ANY, 'Quit', wx.Bitmap('texit.png'))
+		toolbar.Realize()
+		self.Bind(wx.EVT_TOOL, self.OnQuit, qtool)
 
 		panel = wx.Panel(self)
 
-		hbox = wx.BoxSizer(wx.HORIZONTAL)
-
-		fgs = wx.FlexGridSizer(3,2,9,25)
+		fgs = wx.GridBagSizer(5,5)
 		
 		addressTxt = wx.StaticText(panel, label="Address")
 		zipCodeTxt = wx.StaticText(panel, label="Zip Code")
 
 		textbox1 = wx.TextCtrl(panel)
 		textbox2 = wx.TextCtrl(panel)
+		
+		fgs.Add (addressTxt, pos=(1,1), span=(1,2), flag=wx.LEFT|wx.EXPAND)
+		fgs.Add (textbox1, pos=(1,3), span=(1,3), flag=wx.RIGHT|wx.EXPAND) 
+		fgs.Add(zipCodeTxt,pos=(2,1), span=(1,2), flag=wx.LEFT|wx.EXPAND) 
+		fgs.Add(textbox2, pos=(2,3), span=(1,3),flag=wx.RIGHT|wx.EXPAND)
 
-		fgs.AddMany([(addressTxt), (textbox1, 1, wx.EXPAND), (zipCodeTxt), (textbox2, 1, wx.EXPAND)])
+		searchButton = wx.Button(self, label="Search", size=(90,28))
+		fgs.Add(searchButton,pos=(5,5), span=(1,3), flag=wx.RIGHT|wx.BOTTOM, border=5)
 
-		fgs.AddGrowableRow(2,1)
-		fgs.AddGrowableCol(1,1)
-
-		hbox.Add(fgs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
-		panel.SetSizer(hbox)
+		fgs.AddGrowableRow(4)
+		fgs.AddGrowableCol(4)
+		panel.SetSizerAndFit(fgs)
 
 		self.Centre()
 		self.Show(True)
