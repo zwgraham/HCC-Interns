cede78f2fd17ec2a46569fd9eb097d5f6cc595a8
conorkaminer <conorkaminer@gmail.com>
2012-07-26 22:46:58 -0700

    Put a search button in the bottom right hand corner and made textboxes expand..

--- chargepoint/chargeStationGUI.py
+++ chargepoint/chargeStationGUI.py
@@ -23,24 +23,24 @@ class ChargeStationFind(wx.Frame):
 
 		panel = wx.Panel(self)
 
-		fgs = wx.GridBagSizer(5,5)
+		fgs = wx.GridBagSizer(4,4)
 		
 		addressTxt = wx.StaticText(panel, label="Address")
 		zipCodeTxt = wx.StaticText(panel, label="Zip Code")
 
 		textbox1 = wx.TextCtrl(panel)
 		textbox2 = wx.TextCtrl(panel)
-		
+			
+		searchButton = wx.Button(panel, label="Search", size=(90,28))
+
 		fgs.Add (addressTxt, pos=(1,1), span=(1,2), flag=wx.LEFT|wx.EXPAND)
 		fgs.Add (textbox1, pos=(1,3), span=(1,3), flag=wx.RIGHT|wx.EXPAND) 
 		fgs.Add(zipCodeTxt,pos=(2,1), span=(1,2), flag=wx.LEFT|wx.EXPAND) 
 		fgs.Add(textbox2, pos=(2,3), span=(1,3),flag=wx.RIGHT|wx.EXPAND)
+		fgs.Add(searchButton,pos=(5,5), span=(1,3), flag=wx.RIGHT)
 
-		searchButton = wx.Button(self, label="Search", size=(90,28))
-		fgs.Add(searchButton,pos=(5,5), span=(1,3), flag=wx.RIGHT|wx.BOTTOM, border=5)
-
-		fgs.AddGrowableRow(4)
-		fgs.AddGrowableCol(4)
+		fgs.AddGrowableRow(3)
+		fgs.AddGrowableCol(3)
 		panel.SetSizerAndFit(fgs)
 
 		self.Centre()
